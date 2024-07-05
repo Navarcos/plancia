1bf670b9bad3a2f81df450b3d6f6c680fa655357
Andrea Serrecchia <andrea.serrecchia@network-contacts.it>
2024-07-05 16:33:09 +0200

    feat(): added README

--- navarcos-ca.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-NAVARCOS_CA=$(kubectl get secret navarcos-ca -n gitlab -o jsonpath='{.data.navarcos-ca\.crt}'|base64 -d)
-echo "${NAVARCOS_CA}"
--- plancia-url.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-FE_URL=$(kubectl get ingress navarcos-ca -n plancia -o jsonpath='{.spec.rules[0].host}')
-echo "https://${FE_URL}"
