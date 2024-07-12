6a90ba6e1af464c38e427c78952dd12eba28cd12
Andrea Serrecchia <andrea.serrecchia@network-contacts.it>
2024-07-12 11:45:44 +0200

    fix(): import cluster

--- backend/internal/domain/types/external_cluster.go
+++ backend/internal/domain/types/external_cluster.go
@@ -1,8 +1,14 @@
 package types
 
+import "fmt"
+
 type ExternalCluster struct {
 	Name       string
 	Namespace  string
 	Provider   string
 	Kubeconfig string
+}
+
+func (c *ExternalCluster) GetId() string {
+	return fmt.Sprintf("%s-%s", c.Namespace, c.Name)
 }
